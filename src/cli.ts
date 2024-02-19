daf8e38a65290fe0a78365cc298620943af92036
wmxpy <wm@mengw.io>
2024-02-19 13:54:53 -0600

    Add "list" command to CLI and update tsconfig.build.json

--- src/cli.ts
+++ src/cli.ts
@@ -9,4 +9,16 @@ const imbricateProgram = new Command();
 
 imbricateProgram
     .version("<current-version>")
+    .name("imbricate")
     .description("Imbricate CLI");
+
+const listCommand = imbricateProgram.command("list");
+listCommand
+    .description("List all the APIs")
+    .action(() => {
+        console.log("List");
+    });
+
+export const execute = (): void => {
+    imbricateProgram.parse(process.argv);
+};
