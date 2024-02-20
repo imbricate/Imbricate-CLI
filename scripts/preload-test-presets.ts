493ff205697951e37d9ec661de658ec37508fa10
wmxpy <wm@mengw.io>
2024-02-20 13:19:55 -0600

    Refactor test presets to use absolute paths

--- scripts/preload-test-presets.ts
+++ scripts/preload-test-presets.ts
@@ -4,13 +4,35 @@
  * @description Preload Test Presets
  */
 
-import { Folder, digFolder } from "@sudoo/io";
+import { Folder, attemptMarkDir, digFolder, readTextFile, writeTextFile } from "@sudoo/io";
+import * as Path from "path";
 
 (async () => {
 
-    const presetsFolderPath = "./test/presets";
+    const presetsFolderPath = Path.resolve("./test/presets");
+    const nodeModulesFolderPath = Path.resolve("./node_modules/@imbricate-test");
 
     const presetsFolder: Folder = await digFolder(presetsFolderPath);
 
-    console.log(presetsFolder);
+    for (const file of presetsFolder.files) {
+
+        const packageName: string = file.substring(0, file.length - 3);
+        const scriptText = await readTextFile(
+            Path.join(presetsFolderPath, file),
+        );
+
+        console.log(packageName, scriptText);
+
+        await attemptMarkDir(nodeModulesFolderPath);
+        await attemptMarkDir(
+            Path.join(nodeModulesFolderPath, packageName),
+        );
+
+        await writeTextFile(
+            Path.join(nodeModulesFolderPath, packageName, "index.js"),
+            scriptText,
+        );
+
+        console.log(`Preload preset: ${packageName}`);
+    }
 })();
