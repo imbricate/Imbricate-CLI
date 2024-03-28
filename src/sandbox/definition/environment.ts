a5ac34a71e778d6b5c376e53b556c356cd23202f
wmxpy <wm@mengw.io>
2024-03-28 11:59:48 -0500

    Add executeCommand function to sandbox.ts

---
+++ src/sandbox/definition/environment.ts
@@ -0,0 +1,10 @@
+/**
+ * @author WMXPY
+ * @namespace Sandbox_Definition
+ * @description Environment
+ */
+
+export type SandboxEnvironment = {
+
+    readonly time: Date;
+};
--- src/sandbox/sandbox.ts
+++ src/sandbox/sandbox.ts
@@ -5,6 +5,8 @@
  */
 
 import { Sandbox } from "@sudoo/marked";
+import { SandboxExecuteConfig } from "./definition/config";
+import { SandboxEnvironment } from "./definition/environment";
 import { SandboxFeature } from "./feature/feature";
 import { sandboxProvideFeatures } from "./provide/feature";
 
@@ -17,4 +19,21 @@ export const createSandbox = (
     sandboxProvideFeatures(sandbox, features);
 
     return sandbox;
+};
+
+export const executeCommand = async (
+    script: string,
+    features: SandboxFeature[],
+    config: SandboxExecuteConfig,
+): Promise<any> => {
+
+    const sandbox: Sandbox = createSandbox(features);
+
+    sandbox.inject("environment", {
+        time: new Date(),
+    } satisfies SandboxEnvironment);
+
+    sandbox.inject("config", config);
+
+    return await sandbox.evaluate(script);
 };
