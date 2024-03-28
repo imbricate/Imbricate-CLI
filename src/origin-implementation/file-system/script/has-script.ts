19da791babda06086779ef7606842bf0b15ecf6f
wmxpy <wm@mengw.io>
2024-03-28 00:24:22 -0500

    Refactor fileSystemOriginHasScript function

--- src/origin-implementation/file-system/script/has-script.ts
+++ src/origin-implementation/file-system/script/has-script.ts
@@ -4,22 +4,16 @@
  * @description Has Script
  */
 
-import { directoryFiles } from "@sudoo/io";
-import { getScriptsMetadataFolderPath } from "../util/path-joiner";
-import { ensureScriptFolders } from "./common";
+import { ImbricateScriptMetadata } from "../../../definition/script";
+import { fileSystemOriginListScripts } from "./list-scripts";
 
 export const fileSystemOriginHasScript = async (
     basePath: string,
     scriptName: string,
 ): Promise<boolean> => {
 
-    await ensureScriptFolders(basePath);
+    const scriptList: ImbricateScriptMetadata[] =
+        await fileSystemOriginListScripts(basePath);
 
-    const scriptMetadataPath: string = getScriptsMetadataFolderPath(basePath);
-
-    const scriptMetadataFiles: string[] = await directoryFiles(scriptMetadataPath);
-
-    console.log(scriptMetadataFiles);
-
-    return scriptMetadataFiles.includes(scriptName);
+    return scriptList.some((script: ImbricateScriptMetadata) => script.scriptName === scriptName);
 };
