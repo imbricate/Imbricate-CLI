cab4cb469bfe4c8f7b334621122f3d419a763363
wmxpy <wm@mengw.io>
2024-04-07 13:42:42 -0500

    Refactor code to improve performance and readability

---
+++ src/commands/script/rename.ts
@@ -0,0 +1,77 @@
+/**
+ * @author WMXPY
+ * @namespace Commands_Script
+ * @description Rename
+ */
+
+import { IImbricateOrigin, IImbricateScript } from "@imbricate/core";
+import { Command } from "commander";
+import { IConfigurationManager } from "../../configuration/interface";
+import { CLIActiveOriginNotFound } from "../../error/origin/active-origin-not-found";
+import { GlobalManager } from "../../global/global-manager";
+import { cliGetScript } from "../../script/get-script";
+import { ITerminalController } from "../../terminal/definition";
+import { createActionRunner } from "../../util/action-runner";
+import { createConfiguredCommand } from "../../util/command";
+
+type ScriptRenameCommandOptions = {
+
+    readonly quite?: boolean;
+
+    readonly scriptName?: string;
+    readonly identifier?: string;
+};
+
+export const createScriptRenameCommand = (
+    globalManager: GlobalManager,
+    terminalController: ITerminalController,
+    _configurationManager: IConfigurationManager,
+): Command => {
+
+    const showCommand: Command = createConfiguredCommand("show");
+
+    showCommand
+        .description("show details of a existing standalone script")
+        .argument("<new-script-name>", "the new name of the script")
+        .option("-q, --quiet", "quite mode")
+        .option(
+            "-s, --script-name <script-name>",
+            "delete page by script name (one-of)",
+        )
+        .option(
+            "-i, --identifier <script-identifier>",
+            "delete page by script identifier or pointer (one-of)",
+        )
+        .action(createActionRunner(terminalController, async (
+            newScriptName: string,
+            options: ScriptRenameCommandOptions,
+        ): Promise<void> => {
+
+            const currentOrigin: IImbricateOrigin | null = globalManager.findCurrentOrigin();
+
+            if (!currentOrigin) {
+                throw CLIActiveOriginNotFound.create();
+            }
+
+            const script: IImbricateScript = await cliGetScript(
+                currentOrigin,
+                options.scriptName,
+                options.identifier,
+            );
+
+            if (!options.quite) {
+
+                terminalController.printInfo(`Found script "${script.scriptName}" with identifier "${script.identifier}"`);
+            }
+
+            await currentOrigin.renameScript(script.identifier, newScriptName);
+
+
+            if (!options.quite) {
+
+                terminalController.printInfo(`Renamed script "${script.scriptName}" -> "${newScriptName}"`);
+            }
+        }));
+
+    return showCommand;
+};
---
+++ src/script/get-script.ts
@@ -0,0 +1,61 @@
+/**
+ * @author WMXPY
+ * @namespace Script
+ * @description Get Script
+ */
+
+import { IImbricateOrigin, IImbricateScript, ImbricateScriptSnapshot } from "@imbricate/core";
+import { CLIScriptInvalidInput } from "../error/script/script-invalid-input";
+import { CLIScriptNotFound } from "../error/script/script-not-found";
+
+export const cliGetScript = async (
+    origin: IImbricateOrigin,
+    scriptName?: string,
+    identifier?: string,
+): Promise<IImbricateScript> => {
+
+    if (!scriptName && !identifier) {
+        throw CLIScriptInvalidInput.withMessage("One of --script-name or --identifier is required");
+    }
+
+    const scriptSnapshots: ImbricateScriptSnapshot[] = await origin.listScripts();
+
+    if (typeof scriptName === "string") {
+
+        const scriptSnapshot: ImbricateScriptSnapshot | undefined =
+            scriptSnapshots.find((each: ImbricateScriptSnapshot) => {
+                return each.scriptName === scriptName;
+            });
+
+        if (!scriptSnapshot) {
+            throw CLIScriptNotFound.withScriptName(`Script "${scriptName}" not found`);
+        }
+
+        const script = await origin.getScript(scriptSnapshot.identifier);
+
+        if (!script) {
+            throw CLIScriptNotFound.withScriptIdentifier(`Script with identifier "${scriptSnapshot.identifier}" not found`);
+        }
+
+        return script;
+    }
+
+    if (typeof identifier === "string" && identifier.length > 0) {
+
+        for (const each of scriptSnapshots) {
+
+            if (each.identifier.startsWith(identifier)) {
+
+                const script = await origin.getScript(each.identifier);
+
+                if (!script) {
+                    throw CLIScriptNotFound.withScriptIdentifier(`Script with identifier "${each.identifier}" not found`);
+                }
+
+                return script;
+            }
+        }
+    }
+
+    throw CLIScriptNotFound.withScriptIdentifier(`Script with identifier or pointer "${identifier}" not found`);
+};
