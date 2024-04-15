3c6ebbed1fe14b77408f42edf1c5628e4d8cd9db
wmxpy <wm@mengw.io>
2024-04-15 09:31:55 -0500

    Add createScriptAttributeSetCommand to createScriptAttributeCommand in attribute.ts

---
+++ src/commands/script/attribute/set.ts
@@ -0,0 +1,72 @@
+/**
+ * @author WMXPY
+ * @namespace Commands_Script_Attribute
+ * @description Set
+ */
+
+import { IImbricateOrigin, IImbricateScript } from "@imbricate/core";
+import { Command } from "commander";
+import { IConfigurationManager } from "../../../configuration/interface";
+import { CLIActiveOriginNotFound } from "../../../error/origin/active-origin-not-found";
+import { GlobalManager } from "../../../global/global-manager";
+import { cliGetScript } from "../../../script/get-script";
+import { ITerminalController } from "../../../terminal/definition";
+import { createActionRunner } from "../../../util/action-runner";
+import { createConfiguredCommand } from "../../../util/command";
+
+type ScriptAttributeSetCommandOptions = {
+
+    readonly scriptName?: string;
+    readonly identifier?: string;
+
+    readonly quiet?: boolean;
+};
+
+export const createScriptAttributeSetCommand = (
+    globalManager: GlobalManager,
+    terminalController: ITerminalController,
+    _configurationManager: IConfigurationManager,
+): Command => {
+
+    const setCommand: Command = createConfiguredCommand("set");
+
+    setCommand
+        .description("set attribute of a script")
+        .argument("<attribute-key>", "the key of the attribute")
+        .argument("<attribute-value>", "the value of the attribute")
+        .option(
+            "-s, --script-name <script-name>",
+            "delete page by script name (one-of)",
+        )
+        .option(
+            "-i, --identifier <script-identifier>",
+            "delete page by script identifier or pointer (one-of)",
+        )
+        .option("-q, --quiet", "quite mode")
+        .action(createActionRunner(terminalController, async (
+            attributeKey: string,
+            attributeValue: string,
+            options: ScriptAttributeSetCommandOptions,
+        ): Promise<void> => {
+
+            const currentOrigin: IImbricateOrigin | null = globalManager.findCurrentOrigin();
+
+            if (!currentOrigin) {
+                throw CLIActiveOriginNotFound.create();
+            }
+
+            const script: IImbricateScript = await cliGetScript(
+                currentOrigin,
+                options.scriptName,
+                options.identifier,
+            );
+
+            await script.writeAttribute(attributeKey, attributeValue);
+
+            if (!options.quiet) {
+                terminalController.printInfo(`Attribute ${attributeKey} set to ${attributeValue}`);
+            }
+        }));
+
+    return setCommand;
+};
