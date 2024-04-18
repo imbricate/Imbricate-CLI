9f0c8dde5004ab85d82ecf32768c04b6a0fe1233
wmxpy <wm@mengw.io>
2024-04-18 09:49:34 -0500

    Refactor code to use `createScriptSavingTarget` and `createPageSavingTarget` functions for creating saving targets

---
+++ src/editing/editing.ts
@@ -0,0 +1,26 @@
+/**
+ * @author WMXPY
+ * @namespace Editing
+ * @description Editing
+ */
+
+import { readActiveEditing } from "./controller";
+import { SavingTarget } from "./definition";
+import { hashSavingTarget } from "./save-target";
+
+export const checkHasActiveEditing = async (
+    savingTarget: SavingTarget<any>,
+): Promise<boolean> => {
+
+    const activeEditing = await readActiveEditing();
+
+    const savingTargetHash = hashSavingTarget(savingTarget);
+
+    for (const editing of activeEditing) {
+        if (editing.hash === savingTargetHash) {
+            return true;
+        }
+    }
+
+    return false;
+};
---
+++ src/features/start-page-editing.ts
@@ -0,0 +1,111 @@
+/**
+ * @author WMXPY
+ * @namespace Features
+ * @description Start Page Editing
+ */
+
+import { IImbricateOrigin, IImbricateOriginCollection, IImbricatePage, SandboxFeature, SandboxFeatureBuilder } from "@imbricate/core";
+import { IConfigurationManager } from "../configuration/interface";
+import { getProfileFromConfiguration } from "../configuration/profile/get-profile";
+import { ConfigurationProfileManager } from "../configuration/profile/profile-manager";
+import { SAVING_TARGET_TYPE, SavingTarget } from "../editing/definition";
+import { checkHasActiveEditing } from "../editing/editing";
+import { openContentAndMonitor } from "../editing/open-file";
+import { createPageSavingTarget } from "../editing/saving-target/create-saving.target";
+import { GlobalManager } from "../global/global-manager";
+import { ITerminalController } from "../terminal/definition";
+
+type StartPageEditingInput = {
+
+    readonly collection: string;
+    readonly identifier: string;
+
+    readonly initialContent: string;
+
+    readonly handsFree?: boolean;
+};
+
+const createImplementation = (
+    origin: IImbricateOrigin,
+    globalManager: GlobalManager,
+    terminalController: ITerminalController,
+    configurationManager: IConfigurationManager,
+) => {
+
+    return async (
+        input: StartPageEditingInput,
+    ): Promise<void> => {
+
+        if (typeof input.collection !== "string") {
+            throw new Error("Collection is required");
+        }
+
+        if (typeof input.identifier !== "string") {
+            throw new Error("Identifier is required");
+        }
+
+        if (typeof input.initialContent !== "string") {
+            throw new Error("Initial Content is required");
+        }
+
+        const collection: IImbricateOriginCollection | null =
+            await origin.getCollection(input.collection);
+
+        if (!collection) {
+            throw new Error(`Collection [${input.collection}] not found`);
+        }
+
+        const page: IImbricatePage | null = await collection.getPage(input.identifier);
+
+        if (!page) {
+            throw new Error(`Page [${input.identifier}] not found`);
+        }
+
+        const target: SavingTarget<SAVING_TARGET_TYPE.PAGE> = createPageSavingTarget(
+            globalManager,
+            collection.collectionName,
+            page.identifier,
+        );
+
+        const hasActiveEditing: boolean = await checkHasActiveEditing(target);
+
+        if (hasActiveEditing) {
+            throw new Error(`Page [${page.title}] is currently being edited`);
+        }
+
+        const profile: ConfigurationProfileManager = getProfileFromConfiguration(
+            globalManager,
+            terminalController,
+            configurationManager,
+        );
+
+        await openContentAndMonitor(
+            input.initialContent,
+            `${page.title}.md`,
+            target,
+            globalManager,
+            terminalController,
+            profile,
+            input.handsFree ?? true,
+        );
+    };
+};
+
+export const createStartPageEditingFeature = (
+    origin: IImbricateOrigin,
+    globalManager: GlobalManager,
+    terminalController: ITerminalController,
+    configurationManager: IConfigurationManager,
+): SandboxFeature => {
+
+    return SandboxFeatureBuilder.providedByInterface()
+        .withPackageName("page")
+        .withMethodName("startPageEditing")
+        .withImplementation(createImplementation(
+            origin,
+            globalManager,
+            terminalController,
+            configurationManager,
+        ))
+        .build();
+};
