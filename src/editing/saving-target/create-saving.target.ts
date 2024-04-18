231df66026188f51382f3e7f1c687d7d1c0d44f2
wmxpy <wm@mengw.io>
2024-04-18 09:40:44 -0500

    Add createPageSavingTarget and createScriptSavingTarget functions to create saving targets

---
+++ src/editing/saving-target/create-saving.target.ts
@@ -0,0 +1,40 @@
+/**
+ * @author WMXPY
+ * @namespace Editing_SavingTarget
+ * @description Create Saving Target
+ */
+
+import { GlobalManager } from "../../global/global-manager";
+import { SAVING_TARGET_TYPE, SavingTarget } from "./../definition";
+
+export const createPageSavingTarget = (
+    globalManager: GlobalManager,
+    collection: string,
+    pageIdentifier: string,
+): SavingTarget<SAVING_TARGET_TYPE.PAGE> => {
+
+    return {
+
+        type: SAVING_TARGET_TYPE.PAGE,
+        payload: {
+            origin: globalManager.activeOrigin!,
+            collection,
+            identifier: pageIdentifier,
+        },
+    };
+};
+
+export const createScriptSavingTarget = (
+    globalManager: GlobalManager,
+    scriptIdentifier: string,
+): SavingTarget<SAVING_TARGET_TYPE.SCRIPT> => {
+
+    return {
+
+        type: SAVING_TARGET_TYPE.SCRIPT,
+        payload: {
+            origin: globalManager.activeOrigin!,
+            identifier: scriptIdentifier,
+        },
+    };
+};
