3d0f7c925c6f3df5668a7f4230493f076cb5d40f
wmxpy <wm@mengw.io>
2024-04-19 22:27:21 -0500

    Refactor createPageRenderCommand function in page.ts

--- src/commands/page/render.ts
+++ src/commands/page/render.ts
@@ -4,14 +4,13 @@
  * @description Render
  */
 
-import { IImbricateOrigin, IImbricateOriginCollection, IImbricatePage, ImbricatePageSnapshot } from "@imbricate/core";
+import { IImbricateOrigin, IImbricateOriginCollection, IImbricatePage } from "@imbricate/core";
 import { Command } from "commander";
 import { IConfigurationManager } from "../../configuration/interface";
 import { CLICollectionNotFound } from "../../error/collection/collection-not-found";
 import { CLIActiveOriginNotFound } from "../../error/origin/active-origin-not-found";
-import { CLIPageInvalidInput } from "../../error/page/page-invalid-input";
-import { CLIPageNotFound } from "../../error/page/page-not-found";
 import { GlobalManager } from "../../global/global-manager";
+import { cliGetPage } from "../../page/get-page";
 import { ITerminalController } from "../../terminal/definition";
 import { createActionRunner } from "../../util/action-runner";
 import { createConfiguredCommand } from "../../util/command";
@@ -50,83 +49,29 @@ export const createPageRenderCommand = (
             options: PageRenderCommandOptions,
         ): Promise<void> => {
 
-            if (!options.title && !options.identifier) {
-                throw CLIPageInvalidInput.withMessage("One of --title or --identifier is required");
-            }
-
-            const collectionName: string = options.collection;
-
             const currentOrigin: IImbricateOrigin | null = globalManager.findCurrentOrigin();
 
             if (!currentOrigin) {
                 throw CLIActiveOriginNotFound.create();
             }
 
-            const hasCollection: boolean = await currentOrigin.hasCollection(collectionName);
-
-            if (!hasCollection) {
-                throw CLICollectionNotFound.withCollectionName(collectionName);
-            }
-
             const collection: IImbricateOriginCollection | null
-                = await currentOrigin.getCollection(collectionName);
+                = await currentOrigin.getCollection(options.collection);
 
             if (!collection) {
-                throw CLICollectionNotFound.withCollectionName(collectionName);
+                throw CLICollectionNotFound.withCollectionName(options.collection);
             }
 
-            const pages: ImbricatePageSnapshot[] =
-                await collection.listPages();
+            const page: IImbricatePage = await cliGetPage(
+                currentOrigin,
+                options.collection,
+                options.title,
+                options.identifier,
+            );
 
-            if (typeof options.title === "string" && options.title.length > 0) {
+            const content: string = await page.readContent();
 
-                const pageSnapshot: ImbricatePageSnapshot | undefined = pages.find((
-                    each: ImbricatePageSnapshot,
-                ) => {
-                    return each.title === options.title;
-                });
-
-                if (!pageSnapshot) {
-                    throw CLIPageNotFound.withPageTitle(options.title);
-                }
-
-                const page: IImbricatePage | null = await collection.getPage(pageSnapshot.identifier);
-
-                if (!page) {
-                    throw CLIPageNotFound.withPageTitle(options.title);
-                }
-
-                const pageContent: string = await page.readContent();
-
-                terminalController.printInfo(pageContent);
-                return;
-            }
-
-            if (typeof options.identifier === "string" && options.identifier.length > 0) {
-
-                for (const pageSnapshot of pages) {
-
-                    if (pageSnapshot.identifier.startsWith(options.identifier)) {
-
-                        const page: IImbricatePage | null = await collection.getPage(
-                            pageSnapshot.identifier,
-                        );
-
-                        if (!page) {
-                            throw CLIPageNotFound.withPageIdentifier(
-                                pageSnapshot.identifier,
-                            );
-                        }
-
-                        const pageContent: string = await page.readContent();
-
-                        terminalController.printInfo(pageContent);
-                        return;
-                    }
-                }
-
-                throw CLIPageNotFound.withPageIdentifier(options.identifier);
-            }
+            terminalController.printInfo(content);
         }));
 
     return renderCommand;
