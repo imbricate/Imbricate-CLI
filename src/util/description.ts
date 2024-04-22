226975fdf9efc85f071ce2d393d9704bb764bf1f
wmxpy <wm@mengw.io>
2024-04-22 13:20:51 -0500

    Update imbricate CLI description in src/util/description.ts

--- src/util/description.ts
+++ src/util/description.ts
@@ -7,11 +7,11 @@
 export const getApplicationDescription = (): string => {
 
     return [
-        "┌───────┐",
-        "│ I M B │",
-        "│ R I C │",
-        "│ A T E │",
-        "└───────┘",
+        " ┌───────┐ ",
+        " │ I M B │ ",
+        " │ R I C │ ",
+        " │ A T E │ ",
+        " └───────┘ ",
         "",
         "Imbricate CLI",
     ].join("\n");
